2d8c853292b29dd9750c1604a313650a3c3822ff
Patrick Collins <pcollins1@gmail.com>
2018-07-02 15:54:32 -0700

    test mod

--- REA-Rental.js
+++ REA-Rental.js
@@ -2,14 +2,13 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 url = 
-'https://www.realestate.com.au/rent/property-house-with-3-bedrooms-in-aspley%2c+qld+4034/list-1?maxBeds=3&includeSurrounding=false&persistIncludeSurrounding=true&source=location-search'
+'https://www.realestate.com.au/rent/property-house-with-3-bedrooms-in-aspley%2c+qld+4034/list-1?maxBeds=3&source=location-search'
 
 var regex = /\, Aspley.*/
 var suburb = 'Aspley'
 
 request(url, function (error, response, html) {
 	if (!error && response.statusCode == 200) {
-		console.log('Got it');
 		var $ = cheerio.load(html);
 		$('#searchResultsTbl > article').each(function(i, element){
 			var a = $(this).find('div .listingInfo');
@@ -19,7 +18,7 @@ request(url, function (error, response, html) {
 			var bth = $(this).find('div.listingInfo dl dt.rui-icon-bath').next().text();
 			var url = $(this).find('div.vcard h2 a').attr('href');
 			url = 'https://www.realestate.com.au'+url
-			// var b = $('a .name');
+
 			dee = dee.replace(regex, '');
 			// console.log(dee);
 			// console.log('price: ', price);
