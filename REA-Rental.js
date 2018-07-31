2e098825d1a80318f190036c329fc7fdddaddd1a
Patrick Collins <pcollins1@gmail.com>
2018-07-30 22:55:23 -0700

    pre-airsyncer
    new version of rental. about to refactor to use airsyncer module

--- REA-Rental.js
+++ REA-Rental.js
@@ -1,34 +1,112 @@
-var request = require('request');
-var cheerio = require('cheerio');
+"use strict"
 
-url = 
-'https://www.realestate.com.au/rent/property-house-with-3-bedrooms-in-aspley%2c+qld+4034/list-1?maxBeds=3&source=location-search'
+var request = require('request') // for requests
+var cheerio = require('cheerio') // for html parsing
 
-var regex = /\, Aspley.*/
-var suburb = 'Aspley'
+var baseName = "Properties"
+var base = new Airtable({apiKey: 'keyvjbVyJKBdcU2qR'}).base('appNrxhtn5gIpTxUa')
 
-request(url, function (error, response, html) {
-	if (!error && response.statusCode == 200) {
-		var $ = cheerio.load(html);
-		$('#searchResultsTbl > article').each(function(i, element){
-			var a = $(this).find('div .listingInfo');
-			var dee = $(this).find('div.listingInfo a.name').text();
-			var price = $(this).find('div.listingInfo p.priceText').text();
-			var bed = $(this).find('div.listingInfo dl dt.rui-icon-bed').next().text();
-			var bth = $(this).find('div.listingInfo dl dt.rui-icon-bath').next().text();
-			var url = $(this).find('div.vcard h2 a').attr('href');
-			url = 'https://www.realestate.com.au'+url
+const limiter = new Bottleneck({minTime: 1000/15})
+let atCreate = limiter.wrap(base(baseName).create)
+let atUpdate = limiter.wrap(base(baseName).update)
+let atSelect = limiter.wrap(base(baseName).select)
+let throttledEach = limiter.wrap(atSelect.eachPage);
+let atRecords=[];
 
-			dee = dee.replace(regex, '');
-			// console.log(dee);
-			// console.log('price: ', price);
-			// console.log('beds ', bed);
-			// console.log('bath ', bth);
-			// console.log('url ', 'https://www.realestate.com.au'+url);
-			//
-			data = [dee,suburb, price, bed, bth, url]
-			console.log(data.join('\t'))
+console.log(atSelect)
+atSelect({view: "All", pageSize: 10})
+return;
+
+// Fetch all Properties
+function fetchProperties () {
+	atSelect({view: "All", pageSize: 10})
+	.eachPage(function page(records, fetchNextPage) {
+		console.log("selecting from airTable...")
+		records.forEach(function(record) {
+			atRecords.push(record)
 		});
-	}
-});
+		fetchNextPage();
+	
+	}, function done(err) {
+		if (err) { console.error(err); return; }
+	});
+}
+
+fetchProperties().then(function(values) {
+	console.log("done")
+})
+
+
+
+let suburbs = {
+	'Banyo':  'QLD 4014',
+	'Aspley': 'QLD 4034',
+	'Underwood': 'QLD 4119',
+};
+
+for (const suburb in suburbs) {	
+	const postcode = suburbs[suburb]
+	callListingPage(suburb,postcode,1)
+}
+
+function callListingPage(suburb,postcode,page) {
+
+	let url = `https://www.realestate.com.au/rent/property-house-in-${suburb}%2c+${postcode}/list-${page}?includeSurrounding=false`
+
+	// var regex = /\, ${suburb}.*/
+	request(url, function (error, response, html) {
+		if (!error && response.statusCode == 200) {
+
+			// Property name looks like this:
+			//     30 Hutton Road, Aspley, QLD, 4013
+			// We just want the street
+			//     30 Hutton Road
+			let regex = new RegExp('\, ' + suburb + '.*', "gi");
+
+			let $ = cheerio.load(html);
+			let articles = $('#searchResultsTbl > article')
+
+			// If this page contains more than one article, 
+			// then we should check the next page
+			if (articles.length > 0) {
+				callListingPage(suburb,postcode,page+1)
+			}
+
+			// debug
+			console.warn("page %s for %s, %s: %d results (%s)",page,suburb, postcode,articles.length, url)
+
+			articles.each(function(i, element){
+				
+				let a = $(this).find('div .listingInfo');
+				let img = $(this).find('div.photoviewer a img').attr('data-src');
+				let dee = $(this).find('div.listingInfo a.name').text();
+				let price = $(this).find('div.listingInfo p.priceText').text();
+				let bed = $(this).find('div.listingInfo dl dt.rui-icon-bed').next().text();
+				let bth = $(this).find('div.listingInfo dl dt.rui-icon-bath').next().text();
+				let listingUrl = $(this).find('div.vcard h2 a').attr('href');
+				listingUrl = 'https://www.realestate.com.au'+listingUrl
+
+				dee = dee.replace(regex, '');
+
+				let advertised_price = price
+				// remove $350 per week, weekly, p.w, p.w., p/w, 
+				price = price.match(/\d\d\d+/) || "";
+
+				console.log("-------")
+				console.log(dee);
+				console.log('price: "%s" (%s)', price, advertised_price);
+				console.log('beds ', bed);
+				console.log('bath ', bth);
+				console.log('img ', img);
+				console.log('url ', listingUrl);
+				//
+				// let data = [dee,suburb, advertised_price, price, bed, bth, listingUrl]
+				// console.log(data.join('\t'))
+			});
+		} else {
+			console.error("Failed to fetch %s",error)
+		}
+	});
+
+}
 
---
+++ airtableSync.js
@@ -0,0 +1,189 @@
+// Airtable Object Instantiations
+const Airtable = require("airtable");
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: "keyvjbVyJKBdcU2qR"
+});
+const base = Airtable.base("appNrxhtn5gIpTxUa");
+// var base = new Airtable({apiKey: 'keyvjbVyJKBdcU2qR'}).base('appNrxhtn5gIpTxUa')
+
+// Other module imports
+const Bottleneck = require("bottleneck");
+
+// Bottlenecking instantiations
+const bottleneckDict = {};
+const limiter = new Bottleneck({minTime: 1000/15});
+
+// Logging variables
+let numQueued = 0;
+let numInserted = 0;
+let numUpdated = 0;
+let numIgnored = 0;
+
+module.exports = {
+    // Retrieves an entire Airtable with the name `baseName` and `uniqueIDs` are the column names used to create a unique key mapping
+    getAirtable: function (baseName, uniqueIDs, effectiveFields) {
+        let dicSelect = bottleneckDict[baseName + "Select"]; // Pulls up the throttled version of an Airtable select
+
+        return new Promise(async function (resolve, reject) {
+            let recordDic = {};
+            let uniqueIdDic = {};
+
+            let sClause = await dicSelect({});
+            let throttledEach = limiter.wrap(sClause.eachPage);
+
+            throttledEach(function page(records, next) {
+                records.forEach(function (record) {
+                    let recordKey = '';
+
+                    // Creates the dictionary key by just appending the values of each column together in order
+                    uniqueIDs.forEach(function (colName) {
+                        recordKey += record.fields[colName];
+                    });
+
+                    effectiveFields.forEach(function (field) {
+                        if (!record.fields[field]) {
+                            record.fields[field] = false;
+                        }
+                    });
+
+                    recordDic[recordKey] = record.fields; // Mapping to the actual record data
+                    uniqueIdDic[recordKey] = record.id; // Mapping to the unique record ID because Airtable loves dealing with record IDs
+                });
+                next();
+            }, function complete(err) {
+                if (err) {
+                    console.error("getAirtable() error for", baseName, ":", err);
+                    reject(err);
+                } else {
+                    resolve([recordDic, uniqueIdDic, effectiveFields]);
+                }
+            });
+        });
+    },
+
+    // Adds an airtable `obj` to the Airtable with the name `baseName`
+    insertAirtableObj: async function (baseName, obj) {
+        let dicCreate = bottleneckDict[baseName + "Create"]; // Pulls up the throttled version of an Airtable create
+
+        return new Promise(function (resolve, reject) {
+            dicCreate(obj, function (err, record) {
+                if (err) {
+                    // Sometimes airtable's servers mess up and an operation needs to reprocess
+                    if (err.statusCode === 503) {
+                        console.error("An operation has been requeued");
+                        console.error(`${numQueued++} operations have been queued`);
+                        resolve(module.exports.insertAirtableObj(baseName, obj)); // Basically just calls itself again and hope it works this time
+                    } else {
+                        console.error("insertAirtableObj() error for", baseName, ":", err);
+                        console.error("Attempted to add", obj);
+                        reject(err);
+                    }
+                } else {
+                    resolve(record.getId());
+                }
+            });
+        });
+    },
+
+    // Updates an airtable object with the unique `id` in the Airtable with name `baseName` with `obj`
+    updateAirtableObj: async function (baseName, obj, id) {
+        let dicUpdate = bottleneckDict[baseName + "Update"];
+
+        return new Promise(function (resolve, reject) {
+            dicUpdate(id, obj, function (err, record) {
+                if (err) {
+                    // Some more airtable error catching
+                    if (err.statusCode === 503) {
+                        console.error("An operation has been requeued");
+                        console.error(`${numQueued++} operations have been queued`);
+                        resolve(module.exports.updateAirtableObj(baseName, obj, id)); // Trying again
+                    } else {
+                        console.error("updateAirtableObj() error for", baseName, ":", err);
+                        console.error("Attempted to update", id, "with", obj);
+                        reject(err);
+                    }
+                } else {
+                    resolve(record.fields);
+                }
+            });
+        });
+    },
+
+    // Contains the rerouting logic to see if we insert the `obj` or update it instead
+    upsertAirtableObj: async function (baseName, obj, uniqueIDs, cachedTable) {
+        // Notice that this is the same key generation technique as `getAirtable()`
+        let tableKey = '';
+        uniqueIDs.forEach(function (colName) {
+            tableKey += obj[colName];
+        });
+
+        // Using the `tableKey`, checks whether or not it's saved in memory (`cachedTable`)
+        let searchObject = cachedTable[0][tableKey];
+
+        if (typeof searchObject === "undefined") { // Doesn't exist; add it
+            numInserted++; // Used for logging
+            return module.exports.insertAirtableObj(baseName, obj);
+        } else if (objectEquals(searchObject, obj, cachedTable[2])) { // Object in cache is the same as `obj`, skip it
+            numIgnored++; // Used for logging
+            return searchObject;
+        } else { // Object in cache is different from `obj`, update it
+            numUpdated++; // Used for loggingß
+            return module.exports.updateAirtableObj(baseName, obj, cachedTable[1][tableKey]);
+        }
+    },
+
+    // Useful logging variables
+    getUpsertionStats: function () {
+        return [numInserted, numUpdated, numIgnored]
+    },
+
+    // When running two syncing scripts in tandem using the same library in the same directory,
+    // logging variables need to be overwritten
+    resetUpsertionStats: function () {
+        numQueued = 0;
+        numInserted = 0;
+        numUpdated = 0;
+        numIgnored = 0;
+    },
+
+    // The function to create the throttled versions of Airtable methods
+    createBottlenecks: function (baseName) {
+        let bottleneckCreate = limiter.wrap(base(baseName).create);
+        let bottleneckUpdate = limiter.wrap(base(baseName).update);
+        let bottleneckSelect = limiter.wrap(base(baseName).select);
+
+        // Stores all of these new functions in a dictionary so it's fast to pull them up and use them later
+        bottleneckDict[baseName + "Create"] = bottleneckCreate;
+        bottleneckDict[baseName + "Update"] = bottleneckUpdate;
+        bottleneckDict[baseName + "Select"] = bottleneckSelect;
+    },
+
+};
+
+// Iterates over the "effective fields" in which comparisons will be made
+// This is to ensure overwrites do NOT occur when certain fields still need to be finalized
+function objectEquals(oldObj, newObj, effectiveFields) {
+    let oldVals = [];
+    let newVals = [];
+
+    effectiveFields.forEach(function (key) {
+        oldVals.push(oldObj[key]);
+    });
+    effectiveFields.forEach(function (key) {
+        newVals.push(newObj[key]);
+    });
+
+    // The following code is just an array.equals comparison
+    for(let i = 0; i < oldVals.length; i++) {
+        if (flexibleNotEquals(oldVals[i], newVals[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Because of how Airtable functions, some "different" fields are technically the same
+function flexibleNotEquals(oldVal, newVal) {
+    return oldVal !== newVal && !(oldVal === undefined && newVal === false) && !(oldVal === false && newVal === 0);
+}
