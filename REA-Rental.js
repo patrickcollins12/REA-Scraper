e2c9f17d5f0e233cde3f06ff0ae4535c4f98e3b3
Patrick Collins <pcollins1@gmail.com>
2018-07-31 22:40:21 -0700

    working version

--- REA-Rental.js
+++ REA-Rental.js
@@ -2,26 +2,38 @@
 
 var request = require('request') // for requests
 var cheerio = require('cheerio') // for html parsing
+var log = require('loglevel');
+const Airsync = require("./airtableSync");
 
-const airsync = require("./airtableSync");
+// log.trace(msg)
+// log.debug(msg)
+// log.info(msg)
+// log.warn(msg)
+// log.error(msg)
+// This disables all logging below the given level, so that 
+// after a log.setLevel("warn") call log.warn("something") or 
+// log.error("something") will output messages, 
+// but log.info("something") will not.
+log.setLevel("info")
 
-// Table names that are being updated
+// Table name that is being updated
 const tableName = "Properties";
 
 // Airtable can only run 5 operations per second (supposedly)
 // 15 per second runs fine though
-airsync.createBottlenecks(tableName); 
+Airsync.createBottlenecks(tableName); 
 
-const tableIdentifiers = ["Property"];
-const tableEffectives = ["Property", "Location", "Buy or Rent", "Bed", "BR", "Link", "Rent"];
+const tableIdentifiers = ["Property", "Location"];
+const tableEffectives = ["Property", "Location", "Buy or Rent", "First Date", "Last Date", "Bed", "BR", "Link", "Rent", "Price History"];
 let atdata = [];
 
-airsync.getAirtable(tableName, tableIdentifiers, tableEffectives)
+Airsync.getAirtable(tableName, tableIdentifiers, tableEffectives)
 .then(function(values) {
 	atdata = values
 	let len = Object.values(atdata[1]).length;
-	console.log("AirTable records retrieved:", len)
+	log.info("AirTable records retrieved:", len)
 	fetchREAdata(atdata);
+	log.trace(atdata);
 })
 
 let suburbs = {
@@ -34,7 +46,7 @@ function fetchREAdata() {
 	for (const suburb in suburbs) {	
 		const postcode = suburbs[suburb]
 		callListingPage(suburb,postcode,1)
-	}	
+	}
 }
 
 function callListingPage(suburb,postcode,page) {
@@ -61,39 +73,109 @@ function callListingPage(suburb,postcode,page) {
 			}
 
 			// debug
-			console.warn("page %s for %s, %s: %d results (%s)",page,suburb, postcode,articles.length, url)
+			log.info("page %s for %s, %s: %d results (%s)",page,suburb, postcode,articles.length, url)
 
 			articles.each(function(i, element){
 				
+				log.debug("-------")
+
 				let a = $(this).find('div .listingInfo');
 				let img = $(this).find('div.photoviewer a img').attr('data-src');
-				let dee = $(this).find('div.listingInfo a.name').text();
+				let address = $(this).find('div.listingInfo a.name').text();
 				let price = $(this).find('div.listingInfo p.priceText').text();
 				let bed = $(this).find('div.listingInfo dl dt.rui-icon-bed').next().text();
 				let bth = $(this).find('div.listingInfo dl dt.rui-icon-bath').next().text();
 				let listingUrl = $(this).find('div.vcard h2 a').attr('href');
 				listingUrl = 'https://www.realestate.com.au'+listingUrl
 
-				dee = dee.replace(regex, '');
+				address = address.replace(regex, '');
 
 				let advertised_price = price
 				// remove $350 per week, weekly, p.w, p.w., p/w, 
-				price = price.match(/\d\d\d+/) || "";
+				price = price.match(/\d\d\d+/);
+				if (price) {
+					price = Number(price[0])
+				}
 
-				console.log("-------")
-				console.log(dee);
-				console.log('price: "%s" (%s)', price, advertised_price);
-				console.log('beds ', bed);
-				console.log('bath ', bth);
-				console.log('img ', img);
-				console.log('url ', listingUrl);
-				//
-				// let data = [dee,suburb, advertised_price, price, bed, bth, listingUrl]
-				// console.log(data.join('\t'))
-				airsync.upsertAirtableObj(tableName, newSiteObj, tableIdentifiers, atdata)
+				let newobj = {}
+			
+				newobj["Property"] = address
+				newobj["Location"] = suburb
+				newobj["Buy or Rent"] = "Rent"
+				newobj["Bed"] = Number(bed)
+				newobj["BR"] = Number(bth)
+				newobj["Link"]= listingUrl
+				newobj["Rent"]= price
+				newobj["Source"]= "REA Scraper"
+
+				// ---------------------
+				// DATE HISTORY CAPTURE
+
+				// Get the current date in UTC
+				// -> '2018-07-31T18:41:06.440Z'
+				// -> '2018-07-31'
+				let today = new Date().toJSON().split('T')[0]
+				let firstDate;
+
+				// Logic goes here for existing airTable entries:
+				var ee = null
+				if ( address+suburb in atdata[0]) {
+					ee = atdata[0][address+suburb]
+					firstDate = ee['First Listed']
+				}
+
+				// if there was no firstDate in the existing 
+				// entry, then we need to set it
+				if ( ! firstDate ) {
+					firstDate = today
+				}
+
+				newobj["First Listed"] = firstDate
+				newobj["Last Listed"] = today
+
+				// ---------------------
+				// PRICE HISTORY CAPTURE
+				// if the price is new or has changed capture the history
+				let price_day = today + ":$" + price
+
+				if (!ee || 
+					! 'Price History' in ee || 
+					! ee['Price History'] || 
+					ee['Price History'] === ""
+				) {
+					log.trace("Yeah gotta instantiate:", price_day)
+					newobj["Price History"]= price_day
+				}
+
+				// If the new price is zero, null or false, then skip it
+				else if (!price) { 
+					// skip it
+				}
+
+				// if the rents are different store a history
+				else if (ee['Rent'] !== price) {
+					log.trace("Price Day:", price_day)
+					log.trace("Old Rent:", ee['Rent'], typeof(ee['Rent']))
+					log.trace("New Rent:", price, typeof(price))
+					newobj["Price History"] = ee['Price History'] + "\n" + price_day
+				}
+					
+
+				// LOG AND UPDATE IT!
+				log.debug(newobj)
+				
+				// If airtable record was marked as manually entered then 
+				// don't update it under any circumstance
+				if (ee && ee['Source'] === "Manually Entered") {
+					log.debug("Skipping entry because it was previously Manually Entered")
+				} else {
+					Airsync.upsertAirtableObj(tableName, newobj, tableIdentifiers, atdata)
+					log.debug("updating AT with new entry")
+				}
+
 			});
 		} else {
-			console.error("Failed to fetch %s",error)
+			log.error("Failed to fetch %s",error)
 		}
 	});
 }
