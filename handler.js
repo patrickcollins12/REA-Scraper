f50d99c0d159da6b98140d35abc01ab36679d93f
Patrick Collins <pcollins1@gmail.com>
2018-08-07 23:08:22 -0700

    serverless working!

--- handler.js
+++ handler.js
@@ -1,17 +1,29 @@
 'use strict';
+var log = require('loglevel');
+log.setLevel("info")
+
+module.exports.rea = (event, context, callback) => {
+  var rea = require('./REA-ListingDates');
+
+  callback(null, "Running now.");
+};
+
 
 module.exports.hello = (event, context, callback) => {
-  console.log("Testing")
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
 
-  callback(null, response);
+  // console.log("Testing")
+  // log.warn("Blah")
+  // console.log(context)
+  // console.log(event)
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  // const response = {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     message: 'Go Serverless v1.0! Your function executed successfully!',
+  //     input: event,
+  //   }),
+  // };
+
+  callback(null, "Worked great.");
+
 };
